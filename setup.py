44a4c4c5ab57758bb6f8f6fe0fffef91c438d695
Prasad Jivane <prasadjivane@gmail.com>
2024-12-01 16:29:57 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_mern_app",
+    version="1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+)
